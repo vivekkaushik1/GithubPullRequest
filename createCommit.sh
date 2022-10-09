a92217abf905c8c0d25efbbff70e4d511c48a19a
vivekkaushik1 <vivekit388@gmail.com>
2022-10-09 22:58:48 +0530

    Perfor1-1-11

--- createCommit.sh
+++ createCommit.sh
@@ -1,7 +1,7 @@
 string=""
 x=1
 j=1
- while [ $x -le 10 ]
+ while [ $x -le 9000 ]
  do
   rm -rf random*
   touch randomfile$((j+x))
@@ -10,8 +10,8 @@ j=1
   echo $string
   eval "$string"
   x=$[$x+1]
-  #sleep 1.0
-  #git tag -a Pt7.$((j+x)) -m "vversion Pt1.1$((j+x))"
-  #git push --tags origin
+  sleep 60.0
+  git tag -a Pt7.$((j+x)) -m "vversion Pt1.1$((j+x))"
+  git push --tags origin
+  git push
  done
-git push
